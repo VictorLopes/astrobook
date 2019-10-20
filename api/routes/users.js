599b1fb6b7857731d33487a07b9aef0f94d96645
Victor Lopes <victor.lopesms@gmail.com>
2019-10-20 00:42:01 -0300

    Logo and icon for project.

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var UserModel = require('../model/User.model');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+
+  let user = new UserModel();
+  user.insert({
+    name: 'Teste',
+    lastName: 'Teste2',
+    email: 'teste@teste.com',
+    password: '123',
+    profilePhoto: 'exemplo.jpg'
+  });
+  
+  res.send('Ok');
+
+  /* userSchema.methods.findSimilarTypes = function (cb) {
+    return this.model('Animal').find({ type: this.type }, cb);
+  }; */
 });
 
 module.exports = router;
